9523a9e3aa1cbd0f2d84e0faf77579fe0cd51f00
Axel Lanuza <axell@zoomcatalog.com>
2020-07-30 12:54:11 -0500

    Usando props
    Usar props para pasar datos a un components

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Wellcome = (props) => {
+  console.log(props);
+  return <h1>Bienvenido {props.name} a este curso</h1>
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Bienvenidos a este curso
-          </p>
-          <a
+          <Wellcome name='Axel'/>
+          <p
             className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
           >
-            Learn React
-          </a>
+            Vamos a aprender react
+          </p>
         </header>
       </div>
     );
