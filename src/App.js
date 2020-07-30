a78c283549feb032dee651ae288a03820f81c07c
Axel Lanuza <axell@zoomcatalog.com>
2020-07-30 13:52:53 -0500

    Initialize-state
    Cómo inicializar el state de un componente usando Class Properties

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Counter extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     counter: 1
+  //   }
+  // }
+  // haz que ese constructor desaparezca!
+  // usa las nuevas Class Fields para inicializar el state
+  // como te indica este post: http://aprendiendoreact.com/post/como-inicializar-el-state-de-un-componente-con-class-properties/
+  state = {
+    counter: 1
+  }
+
+  render() {
+    const {counter} = this.state;
+    return <span>{counter}</span>
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Bienvenidos a este curso
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Counter></Counter>
         </header>
       </div>
     );
