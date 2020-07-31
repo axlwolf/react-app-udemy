f9259b1f07c8f15f19154b30e63d0f4a4d398d26
Axel Lanuza <axell@zoomcatalog.com>
2020-07-31 13:24:36 -0500

    bind-events
    Bindeo de diferentes eventos soportados por React

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state ={
+      mouseX: 0,
+      mouseY: 0
+    };
+
+    // this.handleMouseMove = this.handleMouseMove.bind(this);
+    // this.printConsole = this.printConsole.bind(this);
+  }
+
+  handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    this.setState({
+      mouseX: clientX,
+      mouseY: clientY
+    });
+  }
+
+  printConsole = (event) => {
+    console.log(event.nativeEvent);
+    console.log(event);
+  }
+
   render() {
+    const {mouseX, mouseY} = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <h4>
+          Eventos
+        </h4>
+        <Button onClick={this.printConsole}>Click Here!</Button>
+        <div
+          onMouseMove={this.handleMouseMove}
+          style={{
+            border: '1px solid red',
+            marginTop: 10,
+            padding: 10,
+            height: '50vh',
+            width: '50%',
+            marginLeft: 'auto',
+            marginRight: 'auto'
+          }}
+        >
           <p>
-            Bienvenidos a este curso
+            {mouseX}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <p>
+            {mouseY}
+          </p>
+        </div>
       </div>
     );
   }
 }
+
+const Button = ({ onClick, children }) => 
+  <button 
+    onClick={onClick}>
+    {children}
+  </button>
 
 export default App;
