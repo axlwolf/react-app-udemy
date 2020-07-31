225d20f0d86bf615842f5299189530e3c8147133
Axel Lanuza <axell@zoomcatalog.com>
2020-07-31 14:37:58 -0500

    event-forms
    Eventos y
    Formularios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Bienvenidos a este curso
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h4>
+          Formularios
+        </h4>
+        <Form />
       </div>
     );
   }
